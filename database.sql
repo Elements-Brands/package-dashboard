ec0cc4429bad9d57fc183dfe5aba0b953d392670
Bill DAlessandro (root) <bill@elementsbrands.com>
2015-07-10 11:50:50 -0400

    added database schema

---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `packages` (
+  `idx` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `aftership_id` varchar(255) DEFAULT NULL,
+  `tracking` varchar(255) DEFAULT NULL,
+  `carrier` varchar(255) DEFAULT NULL,
+  `method` varchar(255) DEFAULT NULL,
+  `shipped` datetime DEFAULT NULL,
+  `delivery` datetime DEFAULT NULL,
+  `delivery_confirmed` tinyint(1) DEFAULT '0',
+  `status` varchar(255) DEFAULT NULL,
+  `shipper` varchar(255) DEFAULT NULL,
+  `destination` varchar(255) DEFAULT NULL,
+  `contents` text,
+  PRIMARY KEY (`idx`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
